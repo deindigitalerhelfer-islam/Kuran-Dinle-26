6dd0b2c47d779e150ed6d7efac8b48e316028c74
deindigitalerhelfer-islam <deindigitalerhelfer@gmail.com>
2026-08-04 14:07:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,13 @@
-const CACHE_NAME = 'ersenbox-v9';
+const CACHE_NAME = 'ersenbox-v10';
 const STATIC_ASSETS = ['./', './index.html'];
-const CACHE_HOSTS = ['cdn.jsdelivr.net','quran.islam-db.com','raw.githubusercontent.com','cdn.islamic.network','fonts.googleapis.com','fonts.gstatic.com'];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(STATIC_ASSETS)));
-    self.skipWaiting();
-});
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
-    self.clients.claim();
-});
+const CACHE_HOSTS = ['cdn.jsdelivr.net','quran.islam-db.com','raw.githubusercontent.com','cdn.islamic.network','fonts.googleapis.com','fonts.gstatic.com','api.qrserver.com'];
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(STATIC_ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', (e) => {
     const url = e.request.url;
     if (CACHE_HOSTS.some((h) => url.includes(h))) {
         e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request).then((response) => {
-            if (response && (response.ok || response.type === 'opaque')) {
-                const copy = response.clone();
-                caches.open(CACHE_NAME).then((c) => c.put(e.request, copy));
-            }
+            if (response && (response.ok || response.type === 'opaque')) { const copy = response.clone(); caches.open(CACHE_NAME).then((c) => c.put(e.request, copy)); }
             return response;
         }).catch(() => caches.match('./index.html'))));
         return;
